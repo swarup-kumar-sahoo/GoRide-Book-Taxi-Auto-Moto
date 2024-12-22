782ec243dad413f11090920ab363035ddfd51a1b
Swarup kumar sahoo <kumarswarup7272@gmail.com>
2024-12-22 17:10:40 +0530

    Updates

---
+++ precache-manifest.47b575702ec35430e0d0a0182bf39238.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9473d58c5b1f4566a7d3661a6129aa",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/index.html"
+  },
+  {
+    "revision": "9cb3adf7107db3ea6dd8",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/css/main.159fe901.chunk.css"
+  },
+  {
+    "revision": "3d6ba1292db773d4a3ee",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/js/2.885fbe67.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "9cb3adf7107db3ea6dd8",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/js/main.82fa2bcd.chunk.js"
+  },
+  {
+    "revision": "236fc7cfd667cef0f248",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/js/runtime~main.1f2a42a4.js"
+  },
+  {
+    "revision": "6c937a56d4a35e0fff08a3f5dd883de2",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/media/goride.6c937a56.png"
+  },
+  {
+    "revision": "5b1fc650d63f86a6eb689b2c18affd2e",
+    "url": "/GoRide-Book-Taxi-Auto-Moto/static/media/logo.5b1fc650.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/GoRide-Book-Taxi-Auto-Moto/precache-manifest.8f7e00942ecee20f884a105a0173305d.js"
+  "/GoRide-Book-Taxi-Auto-Moto/precache-manifest.47b575702ec35430e0d0a0182bf39238.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/GoRide-Book-Taxi-Auto-Moto/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GoRide-Book-Taxi-Auto-Moto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
